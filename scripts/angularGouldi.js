3e4829b58d359f7022de993c15896a8cbfef53bf
AndreG-P <andre.greiner-petter@t-online.de>
2017-10-13 17:33:26 +0900

    Buggy push

--- scripts/angularGouldi.js
+++ scripts/angularGouldi.js
@@ -1,22 +1,33 @@
-angular.module('gouldiApp', ['schemaForm'])
-    .controller('FormController', function($scope) {
-        $scope.schema = {
-            type: "object",
-            properties: {
-                name: { type: "string", minLength: 2, title: "Name", description: "Name or alias" },
-                title: {
-                    type: "string",
-                    enum: ['dr','jr','sir','mrs','mr','NaN','dj']
-                }
-            }
-        };
+var request = new XMLHttpRequest();
+request.open('GET', 'scripts/schema.json', true);
 
-        $scope.form = [
-            "*",
-            {
-                type: "submit",
-                title: "Save"
-            }
-        ];
-        $scope.model = {};
-    });
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var data = JSON.parse(request.responseText);
+	
+	angular.module('gouldiApp', ['schemaForm'])
+    .controller('FormController', function($scope, $http) {
+		$scope.schema = data; 
+			
+			$scope.form = [
+				"*",
+				{
+					type: "submit",
+					title: "Save"
+				}
+			];
+			
+			$scope.model = {};
+		});
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
