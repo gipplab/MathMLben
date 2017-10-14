0a699cec54100704fa95df059008964b3af657e6
Moritz Schubotz <wiki@physikerwelt.de>
2017-10-14 20:00:18 +0900

    Simplify loading from external JSON files

--- scripts/angularGouldi.js
+++ scripts/angularGouldi.js
@@ -1,33 +1,12 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'scripts/schema.json', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var data = JSON.parse(request.responseText);
-	
-	angular.module('gouldiApp', ['schemaForm'])
-    .controller('FormController', function($scope, $http) {
-		$scope.schema = data; 
-			
-			$scope.form = [
-				"*",
-				{
-					type: "submit",
-					title: "Save"
-				}
-			];
-			
-			$scope.model = {};
-		});
-  } else {
-    // We reached our target server, but it returned an error
-
-  }
-};
-
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
+angular
+    .module('gouldiApp', ['schemaForm'])
+    .controller('FormController', function ($scope, $http) {
+        var loadFromJson = function (name) {
+            $http.get("scripts/" + name + ".json").then(function (res) {
+                $scope[name] = res.data;
+            });
+        };
+        loadFromJson('schema');
+        loadFromJson('form');
+        loadFromJson('model');
+    });
