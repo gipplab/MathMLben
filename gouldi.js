0206f3104d96091a8efae97dd78493ed04937075
Moritz Schubotz <wiki@physikerwelt.de>
2017-10-15 14:30:19 +0900

    Transform LaTeX to MathML using vmext-demo

--- gouldi.js
+++ gouldi.js
@@ -4,7 +4,12 @@ var app = express();
 var path = require("path");
 require('app-module-path').addPath(path.join(__dirname + '/node_modules/vmext'));
 require('mathoid/server.js');
-
+// Allow CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')));
 app.use('/scripts', express.static(path.join(__dirname + '/scripts')));
 app.use('/widgets', express.static(path.join(__dirname + '/node_modules/vmext/public/widgets')));
@@ -14,6 +19,7 @@ app.use('/api', require("./node_modules/vmext/api/versions.js"));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/main.html'));
 });
+
 
 app.listen(8080, function () {
     console.log('Started GoUldI on 8080!');
--- scripts/angularGouldi.js
+++ scripts/angularGouldi.js
@@ -12,11 +12,22 @@ angular
 
         var scriptTag = document.createElement('script');
         scriptTag.setAttribute('src', 'widgets/formula-ast-widget.js');
-        $http.get("scripts/sample-eulergamma.mml.xml").then(function (res) {
+        var payload = new FormData();
+        payload.append("latex", " E = mc^4");
+        $http.post('https://vmext-demo.wmflabs.org/math/mathoid', payload,
+            {
+                headers: {
+                    'Content-Type': undefined,
+                    'Accept': 'application/xml' //           transformResponse: []
+
+                }
+            }) .then(function (res) {
             scriptTag.setAttribute('mathml', res.data);
             var container = document.getElementById("ast");
             container.innerHTML = "";
             container.appendChild(scriptTag);
         });
+  //      $http.get("scripts/sample-eulergamma.mml.xml")
+
 
     });
