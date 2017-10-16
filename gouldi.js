c5f1468d0a7bc6fd9c1498b4b3ae551c948ffde7
AndreG-P <andre.greiner-petter@t-online.de>
2017-10-16 17:37:30 +0900

    Included bootstrap

--- gouldi.js
+++ gouldi.js
@@ -2,22 +2,28 @@ require('app-module-path').addPath(__dirname);
 var express = require('express');
 var app = express();
 var path = require("path");
+
 require('app-module-path').addPath(path.join(__dirname + '/node_modules/vmext'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
 var GithubContent = require('github-content');
 require('mathoid/server.js');
+
 // Allow CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
 app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')));
 app.use('/scripts', express.static(path.join(__dirname + '/scripts')));
+app.use('/css', express.static(path.join(__dirname + '/css')));
 app.use('/widgets', express.static(path.join(__dirname + '/node_modules/vmext/public/widgets')));
 app.use('/vendor', express.static(path.join(__dirname + '/node_modules/vmext/public/vendor')));
 app.use('/api', require("./node_modules/vmext/api/versions.js"));
+
 app.post('/get-model', function (req, res) {
     var body = req.body;
     var gc = new GithubContent(body);
@@ -31,11 +37,10 @@ app.post('/get-model', function (req, res) {
 
     });
 });
-// app.post('/save-model', function(req,res){});
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/main.html'));
 });
-
 
 app.listen(8080, function () {
     console.log('Started GoUldI on 8080!');
