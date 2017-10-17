7b75437fe045f5d0f59e527cfaa5e31dab0ba9d6
Moritz Schubotz <wiki@physikerwelt.de>
2017-10-17 17:05:30 +0900

    Use latexml for rendering

--- scripts/angularGouldi.js
+++ scripts/angularGouldi.js
@@ -16,15 +16,26 @@ angular
             scriptTag.setAttribute('src', 'widgets/formula-ast-widget.js');
             var payload = new FormData();
             payload.append("latex", modelValue);
-            $http.post('https://vmext-demo.wmflabs.org/math/mathoid', payload,
+            // $http.post('https://vmext-demo.wmflabs.org/math/mathoid', payload,
+            //     {
+            //         headers: {
+            //             'Content-Type': undefined,
+            //             'Accept': 'application/xml' //           transformResponse: []
+            //
+            //         }
+            //     }).then(function (res) {
+            //     scriptTag.setAttribute('mathml', res.data);
+            //     var container = document.getElementById("ast");
+            //     container.innerHTML = "";
+            //     container.appendChild(scriptTag);
+            // });
+            $http.post('https://vmext-demo.wmflabs.org/math', payload,
                 {
                     headers: {
-                        'Content-Type': undefined,
-                        'Accept': 'application/xml' //           transformResponse: []
-
+                        'Content-Type': undefined
                     }
                 }).then(function (res) {
-                scriptTag.setAttribute('mathml', res.data);
+                scriptTag.setAttribute('mathml', res.data.result);
                 var container = document.getElementById("ast");
                 container.innerHTML = "";
                 container.appendChild(scriptTag);
