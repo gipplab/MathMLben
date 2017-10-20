00abdc2ac1593879a1ce1ece5f0d3dd11bbc4bc5
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2017-10-20 15:54:51 +0900

    Use inputtex from DLMF website

--- helper/transform.js
+++ helper/transform.js
@@ -5,17 +5,24 @@ var directory = '../data';
 var parseAsync = BB.method(JSON.parse);
 var xml2js = BB.promisifyAll(require('xml2js'));
 fs.readdirAsync(directory).map(function (filename) {
-    return fs.readFileAsync(directory + "/" + filename, "utf8").then(function (content) {
-        return parseAsync(content).then(function (json) {
-            if (json.math_inputtex_semantic) {
-                xml2js.parseStringAsync(json.correct_mml).then(function (result) {
-                    json.math_inputtex = result.math.$.alttext;
-                    return fs.writeFileAsync(directory + '/' + filename, JSON.stringify(json, null, 2));
-                }).catch(function (err) {
-                    console.log("Problem in file" + filename);
-                    console.dir(err);
-                });
+    return fs.readFileAsync(directory + "/" + filename, "utf8")
+        .then(function (content) {
+        return parseAsync(content)
+        })
+        .then(function (json) {
+            if (json.math_inputtex_semantic &&  ! json.math_inputtex) {
+                return rp
+                    .get(json.uri.replace('#','.')+'.tex')
+                    .then(function(res){
+                        json.math_inputtex = res;
+                        return fs.writeFileAsync(directory + '/' + filename, JSON.stringify(json, null, 2));
+                    })
+                    .catch(function(err){
+                        json.math_inputtex = '';
+                        console.log("Problem in file" + filename);
+                        return fs.writeFileAsync(directory + '/' + filename, JSON.stringify(json, null, 2));
+                        //console.dir(err);
+                    });
             }
-        });
-    });
+            });
 });
