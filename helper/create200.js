7ce9b8975558a073ef0d354a6a2375d001e2a947
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2017-12-20 00:34:25 +0900

    Add templates for missing samples

--- helper/create200.js
+++ helper/create200.js
@@ -1,7 +1,7 @@
 var BB = require('bluebird');
 var fs = BB.promisifyAll(require("fs"));
 var directory = '../data';
-var list = Array.apply(null, Array(100)).map(function (_, i) {return i;});
+var list = Array.apply(null, Array(13)).map(function (_, i) {return i;});
 BB.map(list, function(x){
-    return fs.writeFileAsync(directory + '/' + (x+201) +'.json', JSON.stringify({}, null, 2));
+    return fs.writeFileAsync(directory + '/' + (x+288) +'.json', JSON.stringify({}, null, 2));
 });
