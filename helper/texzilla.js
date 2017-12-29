886c2707cb67eddcc408bece24a06a7e2b3dea07
AndreG-P <andre.greiner-petter@t-online.com>
2017-12-29 23:58:08 +0900

    Big updates

---
+++ helper/texzilla.js
@@ -0,0 +1,48 @@
+var BB = require('bluebird');
+var fs = BB.promisifyAll(require('fs'));
+var parseAsync = BB.method(JSON.parse);
+
+var goldDir  = '../data';
+var zillaDir = '../data/texzilla';
+
+var pd = require('pretty-data').pd;
+var zilla = require('texzilla');
+
+console.log("Done init...");
+
+if (!fs.existsSync(zillaDir)){
+    console.log("Create zilla dir.")
+    fs.mkdirSync(zillaDir);
+}
+
+fs.readdirAsync(goldDir).map( function(name){
+    var path = goldDir + "/" + name;
+    if ( fs.statSync(path).isFile() ){
+        return fs.readFileAsync(path, "utf8")
+            .then(function (content){
+                return parseAsync(content)
+            })
+            .then(function (json) {
+                var num = name.split(".")[0];
+                console.log();
+                console.log("Parse " + num + ":");
+                try {
+                    var tex = json.math_inputtex;
+                    console.log( "Actual: " + tex );
+                    tex = tex.replace( /%(?:\r\n|\r|\n)/g, "" );
+                    tex = tex.replace( /^(\\\[)?(\\[.,;!]+)*|[.,;!]*(\\[.,;!]+)*(\\])?$/g, "" );
+                    console.log( "Replace:" + tex );
+                    var filtered = zilla.filterString( tex, true );
+                    var mml = zilla.toMathMLString(filtered);
+                    console.log("Successfully parsed " + num);
+                    var prettyMML = pd.xml(mml);
+                    console.log("Successfully prettify MML.");
+                    return fs.writeFileAsync( zillaDir + "/" + num + ".mml", prettyMML );
+                } catch ( err ){
+                    console.log("Skip " + num + ": " + err.message);
+                }
+            });
+    } else {
+        console.log("Skip directory " + name);
+    }
+});
