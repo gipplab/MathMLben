d34009c07cd5bd045a713e468c4f2a9e3576cf89
AndreG-P <andre.greiner-petter@t-online.de>
2018-01-15 19:39:13 +0900

    Update merge gold file

--- helper/mergeFile.js
+++ helper/mergeFile.js
@@ -3,46 +3,39 @@ var fs = BB.promisifyAll(require('fs'));
 var parseAsync = BB.method(JSON.parse);
 
 var goldDir = '../data';
-var zillaDir = '../data/mathematica';
-
-var pd = require('pretty-data').pd;
-var spawn = require('child-process-promise').spawn;
-console.log('Done init...');
-
-if (!fs.existsSync(zillaDir)) {
-  console.log('Create zilla dir.');
-  fs.mkdirSync(zillaDir);
-}
 
 fs.readdirAsync(goldDir).map(function (name) {
   var path = goldDir + '/' + name;
-  if (fs.statSync(path).isFile()) {
-    var num = name.split('.')[0];
-    if ( num > 200 ){
-        console.log('Skip high numbers... ' + num);
-        return;
-    }
+  var num = name.split('.')[0];
 
+  if (fs.statSync(path).isFile() && num <= 200 ) {
     return fs.readFileAsync(path, 'utf8')
       .then(function (content) {
         return parseAsync(content);
       })
       .then(function (json) {
-        //var num = name.split('.')[0];
         console.log();
         console.log('Parse ' + num + ': ' + json.title);
+
         json.formula={
           qID : ''+name.replace(/\.json/,'') || '0',
           math_inputtex : json.math_inputtex,
-          fid : ''+json.fid || '0',
+          fid : (json.fid | -1) + "",
           title : json.title || 'unknown title',
-          oldId : ''+json.oldId || '0'
+          oldId : (json.oldId | -1) + ""
         };
+
+        if ( num < 100 ){
+          var match = /.*?oldid=(\d+)#.*?\.(\d+)/.exec( json.uri );
+          json.formula.oldId = match[1];
+          json.formula.fid = match[2];
+        }
+
         return json;
       });
+  } else if ( num > 200 ) {
+    console.log('Skip high numbers... ' + num);
   } else {
     console.log('Skip directory ' + name);
-    return {};
   }
-})
-  .then(jsons => {return fs.writeFileAsync('gold.json', JSON.stringify(jsons,null,2));});
+}).then(jsons => {return fs.writeFileAsync('gold.json', JSON.stringify(jsons,null,2));});
