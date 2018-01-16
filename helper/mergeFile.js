df3e1670777782ded6c722d3a4ab9089cc0324b5
AndreG-P <andre.greiner-petter@t-online.de>
2018-01-16 17:20:10 +0900

    Solve issues #49 and #43

--- helper/mergeFile.js
+++ helper/mergeFile.js
@@ -4,38 +4,43 @@ var parseAsync = BB.method(JSON.parse);
 
 var goldDir = '../data';
 
-fs.readdirAsync(goldDir).map(function (name) {
-  var path = goldDir + '/' + name;
-  var num = name.split('.')[0];
+fs.readdirAsync(goldDir)
+    .filter(function (name) {
+        var path = goldDir + '/' + name;
+        var isFile = fs.statSync(path).isFile();
+        if (!isFile) console.log("Skip directories.");
+        return fs.statSync(path).isFile()
+    })
+    .map(function (name) {
+        var path = goldDir + '/' + name;
+        var num = name.split('.')[0];
 
-  if (fs.statSync(path).isFile() && num <= 200 ) {
-    return fs.readFileAsync(path, 'utf8')
-      .then(function (content) {
-        return parseAsync(content);
-      })
-      .then(function (json) {
-        console.log();
-        console.log('Parse ' + num + ': ' + json.title);
+        return fs.readFileAsync(path, 'utf8')
+            .then(function (content) {
+                return parseAsync(content);
+            })
+            .then(function (json) {
+                console.log();
+                console.log('Parse ' + num + ': ' + json.title);
 
-        json.formula={
-          qID : ''+name.replace(/\.json/,'') || '0',
-          math_inputtex : json.math_inputtex,
-          fid : (json.fid | -1) + "",
-          title : json.title || 'unknown title',
-          oldId : (json.oldId | -1) + ""
-        };
+                json.formula = {
+                    qID: '' + name.replace(/\.json/, '') || '0',
+                    math_inputtex: json.math_inputtex,
+                    fid: (json.fid | -1) + "",
+                    title: json.title || 'unknown title',
+                    oldId: (json.oldId | -1) + ""
+                };
 
-        if ( num < 100 ){
-          var match = /.*?oldid=(\d+)#.*?\.(\d+)/.exec( json.uri );
-          json.formula.oldId = match[1];
-          json.formula.fid = match[2];
+                if (num < 100) {
+                    var match = /.*?oldid=(\d+)#.*?\.(\d+)/.exec(json.uri);
+                    json.formula.oldId = match[1];
+                    json.formula.fid = match[2];
+                }
+
+                return json;
+            });
+    })
+    .then(jsons => {
+            return fs.writeFileAsync('gold.json', JSON.stringify(jsons, null, 2));
         }
-
-        return json;
-      });
-  } else if ( num > 200 ) {
-    console.log('Skip high numbers... ' + num);
-  } else {
-    console.log('Skip directory ' + name);
-  }
-}).then(jsons => {return fs.writeFileAsync('gold.json', JSON.stringify(jsons,null,2));});
+    );
