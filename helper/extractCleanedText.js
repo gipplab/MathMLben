d46f58089cc557c979c22ff16df3a626775d3128
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2019-01-03 18:34:10 +0100

    Add plain text list of expressions

---
+++ helper/extractCleanedText.js
@@ -0,0 +1,31 @@
+const BB = require('bluebird');
+const fs = BB.promisifyAll(require('fs'));
+
+const goldDir = '../data';
+
+const helpers = require('./helperMethods');
+
+fs.readdirAsync(goldDir)
+    .filter(function (name) {
+        return helpers.fileCheck(goldDir, name);
+    })
+    .map(function (name) {
+        const path = goldDir + '/' + name;
+        const num = name.split('.')[0];
+        return helpers.parseJSON(path)
+            .then(function (json) {
+                console.log();
+                console.log('Parse ' + num + ': ' + json.title);
+                const tex = (json.correct_tex||"")
+                    .replace(/%(?:\r\n|\r|\n)/g, '')
+                    .replace(/^(\\\[)?(\\[.,;!]+)*|[.,;!]*(\\[.,;!]+)*(\\])?$/g, '')
+                    .replace(/\n/g,' ');
+                return {tex, id: parseInt(num)};
+            });
+    })
+    .call("sort", (a, b) => a.id - b.id)
+    .reduce((s, j) => s += `${j.tex} % https://mathmlben.wmflabs.org/${j.id}\n`, "")
+    .then(s => {
+            return fs.writeFileAsync('ps.tex', s);
+        }
+    );
